dca2b4ab854775d1e1a2d6553103c1275245d990
Cesar Espino <danger_95@hotmail.es>
2018-04-21 09:32:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cesarramses/presentacion/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.cesarramses.presentacion;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.QuickContactBadge;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+
+}
